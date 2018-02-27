4ea02c3fc0411a6579580a0c7ecbd1d837bf2234
Rafael Almeida <rafaelcpalmeida@gmail.com>
2018-02-27 19:23:03 +0000

    Improve import data structure to save memory space

--- importer.rb
+++ importer.rb
@@ -5,10 +5,17 @@ require 'redis'
 puts 'Starting import'
 
 redis = Redis.new
+array = []
 
 text = File.open('pt.dic').read
 text.each_line do |line|
-	redis.set(line.strip, line.strip)
+	array << line.strip
 end
+
+data = array.group_by {|s| s[0,1] }.to_hash
+
+data.each { |key, value|
+	redis.set key, value
+}
 
 puts 'Import completed...'
